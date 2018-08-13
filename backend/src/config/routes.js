9ddec7d2201c3861bcd1ce5b2dbd0494c8940eba
tvasconcelos90 <tiagobiker1@gmail.com>
2018-08-12 21:26:15 -0300

    Including protection to routes

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
-    //Define base URL for every routes
-    const router = express.Router()
-    server.use('/api', router)
     
-    //Payment cycles routes
+    /* ROTAS PROTEGIDAS POR TOKEN JWT */
+
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
+
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+    /* ROTAS ABERTAS */
+
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
